b98d0cc0ed2f0ef6049ca5dbefdaae23b9554977
oreakinwole <oreakinwole@gmail.com>
2021-10-10 21:45:35 +0100

    add auth to fetch data from github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const REPOS_QUERY = gql`
+  {
+    search(type: REPOSITORY, query: "language:JavaScript", last: 5) {
+      edges {
+        node {
+          __typename
+        }
+      }
+    }
+  }
+`;
 
 const App = () => {
   const [term, setTerm] = useState('');
+  const {loading, error, data} = useQuery(REPOS_QUERY);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -10,29 +24,31 @@ const App = () => {
 
   const handleSetTerm = ({ target: { value } }) => setTerm(value);
 
+  if (data) console.log(data)
+  if (error) console.log(error.message)
+
   return (
-    <div className="container-fluid">
-      <form onSubmit={handleSearch}>
-        <div className="form-group">
-          <input
-            type="search"
-            className="form-control"
-            placeholder="Enter search term"
-            onChange={handleSetTerm}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" disabled="false">
+    <div className="container-fluid mt-3">
+      <form onSubmit={handleSearch} className="mb-5">
+        <input
+          type="search"
+          className="form-control col-md-3 d-inline mr-3 align-middle"
+          placeholder="Enter search term"
+          onChange={handleSetTerm}
+        />
+
+        <button type="submit" className="btn btn-primary d-inline align-middle">
           Search
         </button>
       </form>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3">Hii</div>
-          <div className="col-md-3">Hii</div>
-          <div className="col-md-3">Hii</div>
-          <div className="col-md-3">Hii</div>
-        </div>
+      {loading && <p>loading...</p>}
+      {error && <p>error...</p>}
+      <div className="row">
+        <div className="col-md-3">Name: </div>
+        <div className="col-md-3">Hii: </div>
+        <div className="col-md-3">Hii: </div>
+        <div className="col-md-3">Hii: </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import './css/index.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = process.env.REACT_APP_OREDEV_GITHUBTOKEN;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
